0696f855c304d381458e58d360d3fe10e7f951bd
ksnaidu <ksnaidudba@gmail.com>
2026-07-01 16:20:19 +0530

    03-variables.sh added

---
+++ shell-practice/03-variables.sh
@@ -0,0 +1,11 @@
+### #!/bin/bash  ## shebang it's the interpreter to check the sysntax and exicute the commands. 
+#!/bin/bash
+
+PERSON1=modi
+PERSON2=musk
+
+echo "$PERSON1:: Hey $PERSON2, How are you?"
+echo "$PERSON2:: Hello $PERSON1, I am fine. How are you doing"
+echo "$PERSON1: I am fine too. What's up?"
+echo "PERSON2:: Nohting, just going to Mars now, will you come?"
+echo "$PERSON1:: Sorry, you carry on! I will come once you come back"
